3f93c429e4c432bbe9dd75fb1a451dc817972461
Michael Große <grosse@cosmocode.de>
2018-01-15 17:17:47 +0100

    chore: switch to webpack as bundler
    We want to be able to extract functions from the current library to make
    it better testable. Webpack does a decent job bundling all the files
    back together again.

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {'pinch-zoom': './src/pinch-zoom'},
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].umd.js',
+        publicPath: '/dist/',
+    },
+    stats: {
+        modules: false,
+        moduleTrace: false,
+    },
+    module: {
+        loaders: [
+            // {
+            //     test: /\.js[x]?$/,
+            //     exclude: /node_modules/,
+            //     loader: 'eslint-loader',
+            //     enforce: 'pre',
+            //     options: {
+            //         fix: true,
+            //     },
+            // },
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: true,
+                    },
+                },
+            },
+        ],
+    },
+};
+
