2684ec71150240ea1baf54eb9a48033c34fda170
Michael Große <grosse@cosmocode.de>
2018-01-17 15:51:50 +0100

    chore: also generate an uncompressed production version
    If the library is used in another project that includes its own
    minifier, then it might be preferable to use an unminified version which
    still doesn't include the source-map.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
+    entry: {'pinch-zoom.uncompressed': './src/pinch-zoom'},
     devtool: 'source-map',
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            exclude: /\.uncompressed\./,
+            parallel: true,
+            sourceMap: true,
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+    ],
 });
