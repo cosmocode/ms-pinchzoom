f03862e043e5729008145c8500c2dd9f18316103
Michael Große <grosse@cosmocode.de>
2018-01-17 15:28:41 +0100

    fix: let scroll detection work on both android and iOS 🙄
    Apparently different modern browser have different ways to provide the
    scroll position of a document.

--- src/pinch-zoom.js
+++ src/pinch-zoom.js
@@ -454,8 +454,10 @@ var definePinchZoom = function () {
          */
         getTouches: function (event) {
             var rect = this.container.getBoundingClientRect();
-            var posTop = rect.top + document.documentElement.scrollTop;
-            var posLeft = rect.left + document.documentElement.scrollLeft;
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+            var posTop = rect.top + scrollTop;
+            var posLeft = rect.left + scrollLeft;
 
             return Array.prototype.slice.call(event.touches).map(function (touch) {
                 return {
